27f09aeb1ca8e3e51c62bbf63998a3b07f522572
reonokiy <reonokiy@nokiy.net>
2025-08-20 16:44:54 +0800

    add signoz

---
+++ tone/services/signoz/init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# check if DATA_DIR environment variable is set
+if [ -z "$DATA_DIR" ]; then
+  echo "DATA_DIR environment variable is not set"
+  exit 1
+fi
+
+script_dir=$(dirname "$(readlink -f "$0")")
+upstream_deploy_dir="$script_dir/upstream/deploy"
+data_deploy_dir="$DATA_DIR/signoz/deploy"
+
+# make sure the data deploy directory exists
+mkdir -p "$data_deploy_dir"
+cp -r "$upstream_deploy_dir/." "$data_deploy_dir"
