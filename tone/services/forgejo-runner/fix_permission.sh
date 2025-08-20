61cca8c929100eee772a2f0c035a786601a6ef5d
reonokiy <reonokiy@nokiy.net>
2025-08-20 15:16:50 +0800

    add forgejo runner

---
+++ tone/services/forgejo-runner/fix_permission.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# check if DATA_DIR environment variable is set
+if [ -z "$DATA_DIR" ]; then
+  echo "DATA_DIR environment variable is not set"
+  exit 1
+fi
+
+# forgejo-runner data dir
+forgejo_runner_data_dir=${DATA_DIR}/forgejo-runner
+forgejo_runner_cache_dir=${forgejo_runner_data_dir}/.cache
+forgejo_runner_config_dir=${forgejo_runner_data_dir}/.runner
+
+# fix permissions
+sudo chown -R 1001:1001 ${forgejo_runner_cache_dir}
+sudo chown -R 1001:1001 ${forgejo_runner_config_dir}
+sudo chmod 755 ${forgejo_runner_cache_dir}
+sudo chmod 755 ${forgejo_runner_config_dir}
+sudo chmod g+s ${forgejo_runner_cache_dir}
+sudo chmod g+s ${forgejo_runner_config_dir}
