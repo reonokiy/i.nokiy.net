fcc10c60e94923dd4bd94d11bcd6b09121ddfd4a
reonokiy <reonokiy@nokiy.net>
2025-09-04 13:40:21 +0800

    update mongo

---
+++ yuzu/services/mongo/up.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# request for DATA_DIR, default to /data
+read -p "Enter DATA_DIR (default: /data/mongo): " INPUT_DIR
+DATA_DIR=$(realpath "${INPUT_DIR:-/data/mongo}")
+
+# if DATA_DIR does not exist, create it
+if [ ! -d "$DATA_DIR" ]; then
+  sudo mkdir -p "$DATA_DIR"
+fi
+
+op run --env-file=./.env.op -- docker compose up -d
