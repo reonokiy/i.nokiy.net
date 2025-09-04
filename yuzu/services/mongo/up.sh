d7a84d09a9be047514171b18cf83fe1e1e4615c8
reonokiy <reonokiy@nokiy.net>
2025-09-04 13:57:39 +0800

    update DATA_DIR ENV

--- yuzu/services/mongo/up.sh
+++ yuzu/services/mongo/up.sh
@@ -3,6 +3,7 @@
 # request for DATA_DIR, default to /data
 read -p "Enter DATA_DIR (default: /data/mongo): " INPUT_DIR
 DATA_DIR=$(realpath "${INPUT_DIR:-/data/mongo}")
+export DATA_DIR
 
 # if DATA_DIR does not exist, create it
 if [ ! -d "$DATA_DIR" ]; then
